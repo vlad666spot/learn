7553f67b8e69db35df52add53605f3656d196b13
vlad666spot <vladyslav.z@spot.io>
2021-07-24 16:51:28 +0300

    Implemented task that return shouldWake value - when dog is barking from 22 till 8 hours

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,33 @@ public class Main {
 
     public static void main(String[] args) {
         //here we call methods from lines 10 and 15)
-        printMegaBytesAndKiloBytes(0);
+        System.out.println(shouldWakeUp(true, 44));
     }
 
         // write your code here
-    public static void printMegaBytesAndKiloBytes (int kiloBytes){
-        if (kiloBytes<0){
-            System.out.println("Invalid Value");
-        }
-        else {
-            int mbOfKiloBytes = kiloBytes / 1024;
-            int remain = kiloBytes % 1024;
-            System.out.println(kiloBytes + " KB = " + mbOfKiloBytes + " MB and " + remain + " KB");
-        }
+        public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+            boolean shouldWake = false;
+            if (!barking){
+                shouldWake = false;
+            }
+
+            if (barking){
+                if (hourOfDay>23){
+                    shouldWake = false;
+                }
+                if (hourOfDay<0){
+                    shouldWake = false;
+                }
+                if (hourOfDay>=8){
+                    shouldWake = false;
+                }
+                if (hourOfDay>=0 && hourOfDay<8){
+                    shouldWake = true;
+                }
+                if (hourOfDay>22 && hourOfDay<24){
+                    shouldWake = true;
+                }
+            }
+            return shouldWake;
     }
-
-
-
 }
