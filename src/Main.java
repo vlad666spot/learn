a77a0efba9395ba66d7f54d8882256800691a6e4
vlad666spot <vladyslav.z@spot.io>
2021-07-24 14:56:54 +0300

    Coding Exercise 1: Speed Converter

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
-
         //here we call methods from lines 10 and 15)
-        displayHighScorePosition("Vlad", calculateHighScorePosition(3));
+        printConversion(10.25);
     }
 
-    //create method:
-    public static void displayHighScorePosition(String playersName, int position){
-        System.out.println(playersName + " managed to get into position " + position + " on the high score table." );
-    }
-
-    //create method:
-    public static int calculateHighScorePosition(int playerScore){
-        if (playerScore>1000){
-            return 1;
-        } else if (playerScore>500 && playerScore<1000){
-            return 2;
-        } else if (playerScore>100 && playerScore<500){
-            return 3;
-        } else {
-            return 4;
+        // write your code here
+        public static void printConversion (double kilometersPerHour){
+            if (kilometersPerHour<0){
+                System.out.println("Invalid Value");
+            } else {
+                double milesPerHour = toMilesPerHour(kilometersPerHour);
+                System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h.");
+            }
         }
-    }
+
+        public static long toMilesPerHour (double kilometersPerHour){
+        long miles = Math.round(kilometersPerHour/1.609);
+
+        if (miles<0){
+            return -1;
+        } else {
+            return miles;
+        }}
+
+
+
 }
