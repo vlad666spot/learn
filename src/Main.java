582a31665b47840ce4181e5f7e05fcd1f0ad34e0
vlad666spot <vladyslav.z@spot.io>
2021-08-01 12:46:38 +0300

    created Decimal Comparator

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
 public class Main {
 
+    /*
+    * Write a method "areEqualByThreeDecimalPlaces"  of type double. The method should return boolean and it needs to return true if two double numbers are the same up to three decimal places. Otherwise,
+    * return false
+    */
     public static void main(String[] args) {
         //here we call methods from lines 10 and 15)
-        System.out.println(isLeapYear(1924));
+
+        areEqualByThreeDecimalPlaces(2.2, 5.2);
     }
 
-        // write your code here
-        public static boolean isLeapYear (int year){
-        boolean isLeapYear = false;
-            if (year>1 && year<=9999){
-                if(year%4==0){
-                    if (year%100==0){
-                        if (year%400==0){
-                            isLeapYear = true;
-                        }}else {
-                        isLeapYear = true;
-                    }
-                }
-            } else {
-                isLeapYear = false;
-            }
-            return isLeapYear;
+  public static boolean areEqualByThreeDecimalPlaces(double first, double second){
+        String firstDoubleToString = String.valueOf(first);
+        int indexFirstOfDecimal = firstDoubleToString.indexOf(".");
+        int fistInt = Integer.parseInt(firstDoubleToString.substring(indexFirstOfDecimal,3).replace(".", ""));
+
+        String secondDoubleToString = String.valueOf(second);
+        int indexSecondOfDecimal = secondDoubleToString.indexOf(".");
+        int secondInt = Integer.parseInt(secondDoubleToString.substring(indexSecondOfDecimal,3).replace(".", ""));
+
+        if (fistInt == secondInt){
+            return true;
+        } else {
+            return false;
         }
+  }
 }
+
