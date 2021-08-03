4a728ee199c1efe4391713fe4ebc1055446eab40
vlad666spot <vladyslav.z@spot.io>
2021-08-03 19:10:06 +0300

    Implemented Area Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,28 @@
 public class Main {
-
     public static void main(String[] args) {
         //here we call methods from lines 10 and 15)
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
 
-        System.out.println(getDurationString(63, 59));
-        System.out.println(getDurationString(321));
     }
 
-    public static String getDurationString(int minutes, int seconds){
-        if(minutes<0 || seconds<0 || seconds>59){
-            return "Invalid value";
+
+    public static double area (double radius){
+        if(radius<0){
+            return -1.0;
         }
-        int calculatedHours;
-        int calculatedMinutes;
-
-        calculatedHours = minutes/60;
-        calculatedMinutes = minutes%60;
-
-        return calculatedHours+"h "+calculatedMinutes+"m "+seconds+"s";
+        double circleArea = radius*radius*Math.PI;
+        return circleArea;
     }
 
-    public static String getDurationString(long seconds){
-        if(seconds<0){
-            return "Invalid value";
+    public static double area (double x, double y){
+        if(x<0 || y<0){
+            return -1.0;
         }
-
-        int calculatedMinutes;
-        int calculatedSeconds;
-
-        calculatedMinutes = (int)seconds/60;
-        calculatedSeconds = (int)seconds%60;
-
-        return getDurationString(calculatedMinutes,calculatedSeconds);
+        double rectangleArea = x*y;
+        return rectangleArea;
     }
 
 }
