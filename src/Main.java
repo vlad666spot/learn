2f0031cf0b0f86a123df59c23a0f27fea1694fa0
vlad666spot <vladyslav.z@spot.io>
2021-08-03 18:24:21 +0300

    Implemented Seconds And Minutes challenge

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,35 @@ public class Main {
     public static void main(String[] args) {
         //here we call methods from lines 10 and 15)
 
-        calcFeetAndInchesToCentimeters(6, 0);
-        calcFeetAndInchesToCentimeters(5);
+        System.out.println(getDurationString(63, 59));
+        System.out.println(getDurationString(321));
     }
 
-    //here we define "calcFeetAndInchesToCentimeters" method, with 2 parameters
-    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
-        double feetAndInchesToCentimiters = 0;
-        if (feet >= 0) {
-            if (inches >= 0 && inches <= 12) {
-                double oneFootinCM = 30.48;
-                double oneInchInCM = 2.54;
-                feetAndInchesToCentimiters = feet*oneFootinCM + inches*oneInchInCM;
-                System.out.println(feetAndInchesToCentimiters);
-            } else {
-                return -1;
-            }
-        } else {
-            return -1;
+    public static String getDurationString(int minutes, int seconds){
+        if(minutes<0 || seconds<0 || seconds>59){
+            return "Invalid value";
+        }
+        int calculatedHours;
+        int calculatedMinutes;
+
+        calculatedHours = minutes/60;
+        calculatedMinutes = minutes%60;
+
+        return calculatedHours+"h "+calculatedMinutes+"m "+seconds+"s";
+    }
+
+    public static String getDurationString(long seconds){
+        if(seconds<0){
+            return "Invalid value";
         }
 
-    return feetAndInchesToCentimiters;
+        int calculatedMinutes;
+        int calculatedSeconds;
+
+        calculatedMinutes = (int)seconds/60;
+        calculatedSeconds = (int)seconds%60;
+
+        return getDurationString(calculatedMinutes,calculatedSeconds);
     }
 
-    //here we overload method "calcFeetAndInchesToCentimeters", main differences from previous - is other parameters
-    public static double calcFeetAndInchesToCentimeters(double inches){
-        if (inches < 0) {
-            return -1;
-        }
-        return calcFeetAndInchesToCentimeters(0,inches);
-    }
 }
