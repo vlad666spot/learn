8fbd9422f84a59735ee4c7012f9f433a05b2b3f9
vlad666spot <vladyslav.z@spot.io>
2021-08-18 19:57:30 +0300

    Implemented number in word

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,44 @@ public class Main {
 
 
     public static void main(String[] args) {
-        printDayOfTheWeek(-2);
+        printNumberInWord(-2);
 }
 
-    public static void printDayOfTheWeek (int day){
-        if(day<0 || day>6){
-            System.out.println("Invalid day");
+    public static void printNumberInWord(int number){
+        if(number<0 || number>9){
+            System.out.println("other");
         }
+        switch (number){
 
-        switch(day){
+            case 0:
+                System.out.println("ZERO");
+                break;
             case 1:
-                System.out.println("Monday");
+                System.out.println("ONE");
                 break;
             case 2:
-                System.out.println("Tuesday");
+                System.out.println("TWO");
                 break;
             case 3:
-                System.out.println("Wednesday");
+                System.out.println("THREE");
                 break;
             case 4:
-                System.out.println("Thursday");
+                System.out.println("FOUR");
                 break;
             case 5:
-                System.out.println("Friday");
+                System.out.println("FIVE");
                 break;
             case 6:
-                System.out.println("Sunday");
+                System.out.println("SIX");
                 break;
             case 7:
-                System.out.println("Saturday");
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
                 break;
         }
     }
