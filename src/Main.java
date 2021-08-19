1850b2c1abf7ec06c3819139d59e402073f4694a
vlad666spot <vladyslav.z@spot.io>
2021-08-19 13:53:26 +0300

    implemented number of days in month

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,92 @@ public class Main {
 
 
     public static void main(String[] args) {
-        printNumberInWord(-2);
+
+        System.out.println(getDaysInMonth(3,2020));
+
+        System.out.println(isLeapYear(0));
 }
 
-    public static void printNumberInWord(int number){
-        if(number<0 || number>9){
-            System.out.println("other");
+    public static boolean isLeapYear(int year){
+        if(year<=0 || year>9999){
+            return false;
         }
-        switch (number){
 
-            case 0:
-                System.out.println("ZERO");
-                break;
-            case 1:
-                System.out.println("ONE");
-                break;
-            case 2:
-                System.out.println("TWO");
-                break;
-            case 3:
-                System.out.println("THREE");
-                break;
-            case 4:
-                System.out.println("FOUR");
-                break;
-            case 5:
-                System.out.println("FIVE");
-                break;
-            case 6:
-                System.out.println("SIX");
-                break;
-            case 7:
-                System.out.println("SEVEN");
-                break;
-            case 8:
-                System.out.println("EIGHT");
-                break;
-            case 9:
-                System.out.println("NINE");
-                break;
+        if (year%4==0 && year%100!=0 || year%400==0){
+            return true;
+        } else {
+            return false;
         }
+
+    }
+
+    public static int getDaysInMonth (int month, int year) {
+
+        if (month < 1 || month > 12) {
+            return -1;
+        }
+
+        if (year < 1 || year > 9999) {
+            return -1;
+        }
+
+        boolean isLeap = isLeapYear(year);
+        if (isLeap == true) {
+            switch (month) {
+                case 1:
+                    return 31;
+                case 2:
+                    return 29;
+                case 3:
+                    return 31;
+                case 4:
+                    return 30;
+                case 5:
+                    return 31;
+                case 6:
+                    return 30;
+                case 7:
+                    return 31;
+                case 8:
+                    return 31;
+                case 9:
+                    return 30;
+                case 10:
+                    return 31;
+                case 11:
+                    return 30;
+                case 12:
+                    return 31;
+            }
+            return -1;
+        } else {
+            switch (month) {
+                case 1:
+                    return 31;
+                case 2:
+                    return 28;
+                case 3:
+                    return 31;
+                case 4:
+                    return 30;
+                case 5:
+                    return 31;
+                case 6:
+                    return 30;
+                case 7:
+                    return 31;
+                case 8:
+                    return 31;
+                case 9:
+                    return 30;
+                case 10:
+                    return 31;
+                case 11:
+                    return 30;
+                case 12:
+                    return 31;
+            }
+        }
+        return -1;
     }
 }
