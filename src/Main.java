f67dac6c5f6b552f77d79b84958943d1f6e3d91a
vlad666spot <vladyslav.z@spot.io>
2021-08-24 18:42:46 +0300

    implemented get Even Digit Sum , on my own

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,24 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(getEventDigitSum(123456789));
     }
-
-    public static int sumFirstAndLastDigit(int number){
+    public static int getEventDigitSum(int number){
         if(number<0){
             return -1;
         }
-        int num=number;
-        int lastDigit = num%10;
-        while(num>9){
-            num = num/10;
+
+
+        int sum = 0;
+        int numberDecreasing = number;
+
+        while (numberDecreasing>0){
+            int lastDigit = numberDecreasing%10;
+                if (lastDigit%2==0){
+                    sum += lastDigit;
+                }
+            numberDecreasing = numberDecreasing/10;
         }
-        return lastDigit+num;
+        return sum;
     }
 }
