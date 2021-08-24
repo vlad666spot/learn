b559fb8aa3c1efb7a4587f5bcef446e61fd89026
vlad666spot <vladyslav.z@spot.io>
2021-08-24 17:48:28 +0300

    copied solution

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,18 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int number = 4;
-        int finishNumber = 20;
-        int counter = 0;
-        while(number<=finishNumber){
-            number++;
-            if(!isEvenNumber(number)){
-                continue;
-            }
-            System.out.println("Even number "+ number);
-            counter++;
-            if (counter>=5){
-                break;
-            }
-        }
-        System.out.println("sum is " + counter);
+        System.out.println(sumFirstAndLastDigit(5));
     }
 
-    public static boolean isEvenNumber(int number){
-        if (number%2==0){
-            return true;
-        } else {
-            return false;
+    public static int sumFirstAndLastDigit(int number){
+        if(number<0){
+            return -1;
         }
+        int num=number;
+        int lastDigit = num%10;
+        while(num>9){
+            num = num/10;
+        }
+        return lastDigit+num;
     }
 }
