99055af9497bc11354feb29d15ef6811aea682be
vlad666spot <vladyslav.z@spot.io>
2021-08-25 11:14:59 +0300

    Implementedlast digit checker

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,31 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(hasSharedDigit(22, 72));
+        System.out.println(hasSameLastDigit(12, 19, 119));
+        System.out.println(isValid(9));
+
     }
-    public static boolean hasSharedDigit (int one, int two){
-        if(one<11 && one>-100 || two<11 && two>-100){
+    public static boolean hasSameLastDigit (int one, int two, int three){
+        if(one<10 || one>1000 || two<10 || two>1000 || three<10 || three>1000){
             return false;
         }
 
-        int lastNumberOfOne = one%10;
-        int firstNumberOfOne = one/10;
-        int lastNumberOfTwo = two%10;
-        int firstNumberOfOTwo = two/10;
+        int lastOne = one%10;
+        int lastTwo = two%10;
+        int lastThree = three%10;
 
-        if (lastNumberOfOne == lastNumberOfTwo || lastNumberOfOne==firstNumberOfOTwo || firstNumberOfOne== firstNumberOfOTwo || firstNumberOfOne ==lastNumberOfTwo){
+        if (lastOne == lastTwo || lastTwo == lastThree || lastThree ==lastOne){
             return true;
         } else {
+            return false;
+        }
+    }
+
+    public static boolean isValid (int number){
+        if(number>9 && number <1001){
+            return true;
+        }
+        else {
             return false;
         }
     }
