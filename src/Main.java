62600b6393bcfdc7cdfa221f11caab0fe9ae9d75
vlad666spot <vladyslav.z@spot.io>
2021-08-25 13:55:03 +0300

    Greatest common divsor

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,56 @@
+import java.util.ArrayList;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(hasSameLastDigit(12, 19, 119));
-        System.out.println(isValid(9));
-
-    }
-    public static boolean hasSameLastDigit (int one, int two, int three){
-        if(one<10 || one>1000 || two<10 || two>1000 || three<10 || three>1000){
-            return false;
-        }
-
-        int lastOne = one%10;
-        int lastTwo = two%10;
-        int lastThree = three%10;
-
-        if (lastOne == lastTwo || lastTwo == lastThree || lastThree ==lastOne){
-            return true;
-        } else {
-            return false;
-        }
+        System.out.println(getGreatestCommonDivisor(81,153));
     }
 
-    public static boolean isValid (int number){
-        if(number>9 && number <1001){
-            return true;
+
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first < 10 || second < 10) {
+            return -1;
         }
-        else {
-            return false;
+
+        ArrayList<Integer> firstArray = new ArrayList<Integer>();
+        ArrayList<Integer> secondArray = new ArrayList<Integer>();
+        ArrayList<Integer> biggest = new ArrayList<>();
+        for (int i = 1; i <= first; i++) {
+
+            if (first % i == 0) {
+                firstArray.add(i);
+                //System.out.println(i);
+            }
         }
+
+        for (int i = 1; i <= second; i++) {
+
+            if (second % i == 0) {
+                secondArray.add(i);
+                //System.out.println(i);
+            }
+        }
+
+        for (int i = 0; i < firstArray.size(); i++) {
+            int numberOfFirstArray = firstArray.get(i);
+            for (int j = 0; j < secondArray.size(); j++) {
+                int numberOfSecondArray = secondArray.get(j);
+                if (numberOfFirstArray==numberOfSecondArray){
+                    biggest.add(numberOfFirstArray);
+                }
+            }
+        }
+
+        int bigVal = 0;
+        for (int i = 0; i < biggest.size(); i++) {
+            biggest.get(i);
+            if (biggest.get(i)>bigVal){
+                bigVal=biggest.get(i);
+            }
+        }
+
+    return bigVal;
     }
 }
