9c76cac083f343a76d0a88a802dd4a4df782ad88
vlad666spot <vladyslav.z@spot.io>
2021-08-25 19:07:00 +0300

    Implemented number to words

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,80 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(isPerfectNumber(-1));
+        numberToWords(1000);
+        //reverse(234);
     }
 
-    public static boolean isPerfectNumber(int number){
-        if(number<1){
-            return false;
+    public static void numberToWords (int number){
+        if(number<0){
+            System.out.println("Negative Value");
         }
 
-        int sumOfDividers = -number;
-        for (int i = 1; i <= number; i++) {
-            if(number%i==0){
-                sumOfDividers += i;
-                //System.out.println(i+" ");
+        int counter = getDigitCount(number);
+        int copyOfNumber =reverse(number);
+
+        for(int i = 0; i<counter; i++){
+            int lastDigitOfNumber = copyOfNumber%10;
+            switch (lastDigitOfNumber){
+                case 0:
+                    System.out.print("Zero ");
+                    break;
+                case 1:
+                    System.out.print("One ");
+                    break;
+                case 2:
+                    System.out.print("Two ");
+                    break;
+                case 3:
+                    System.out.print("Three ");
+                    break;
+                case 4:
+                    System.out.print("Four ");
+                    break;
+                case 5:
+                    System.out.print("Five ");
+                    break;
+                case 6:
+                    System.out.print("Six ");
+                    break;
+                case 7:
+                    System.out.print("Seven ");
+                    break;
+                case 8:
+                    System.out.print("Eight ");
+                    break;
+                case 9:
+                    System.out.print("Nine ");
+                    break;
             }
+
+            copyOfNumber = copyOfNumber/10;
         }
-        System.out.println("Sum of dividers " + sumOfDividers);
-
-        boolean isPerfectNumber = (sumOfDividers==number);
-
-        if (isPerfectNumber==true){
-            return true;
-        } else {
-        return false;
-    }
     }
 
-    //perfercNumber = divisor + divisoe
-    //divisor %= perfercrNUmber;
+    public static int reverse (int number){
+        int lastDigit;
+        String returnValure = "";
+        while(number>0){
+            lastDigit = number%10;
+            number = number/10;
+            returnValure += ""+lastDigit;
+        }
+        return Integer.parseInt(returnValure);
+    }
+
+    public static int getDigitCount(int number){
+        if(number<0){
+            return -1;
+        }
+
+        int countOfDigitsInNumber = 0;
+        while(number!=0){
+            countOfDigitsInNumber++;
+            number = number/10;
+        }
+
+        return countOfDigitsInNumber;
+
+    }
 }
