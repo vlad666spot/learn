e20fef5f047f0027ed707ce4d64fc06e21dd67ee
vlad666spot <vladyslav.z@spot.io>
2021-08-25 09:45:18 +0300

    Implemented Shared Digit

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,22 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(hasSharedDigit(22, 72));
     }
-    public static int getEvenDigitSum(int number){
-        if(number<0){
-            return -1;
+    public static boolean hasSharedDigit (int one, int two){
+        if(one<11 && one>-100 || two<11 && two>-100){
+            return false;
         }
 
+        int lastNumberOfOne = one%10;
+        int firstNumberOfOne = one/10;
+        int lastNumberOfTwo = two%10;
+        int firstNumberOfOTwo = two/10;
 
-        int sum = 0;
-        int numberDecreasing = number;
-
-        while (numberDecreasing>0){
-            int lastDigit = numberDecreasing%10;
-                if (lastDigit%2==0){
-                    sum += lastDigit;
-                }
-            numberDecreasing = numberDecreasing/10;
+        if (lastNumberOfOne == lastNumberOfTwo || lastNumberOfOne==firstNumberOfOTwo || firstNumberOfOne== firstNumberOfOTwo || firstNumberOfOne ==lastNumberOfTwo){
+            return true;
+        } else {
+            return false;
         }
-        return sum;
     }
-
-    /* EXPLANATION
-    * value / 10 = remove one char (last)
-    * value * 10 = add one char (last, 0)
-    * value % 10 = take last char
-    *
-    *
-    *
-    * */
 }
