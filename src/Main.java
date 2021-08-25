2507e10ecf89d5e6d6eace427ed9baadb959a225
vlad666spot <vladyslav.z@spot.io>
2021-08-25 15:02:46 +0300

    Implemented Perfect Number

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
-import java.util.ArrayList;
-
 public class Main {
 
 
     public static void main(String[] args) {
-        printFactors(-1);
+        System.out.println(isPerfectNumber(-1));
     }
-    
-    public static void printFactors (int number){
+
+    public static boolean isPerfectNumber(int number){
         if(number<1){
-            System.out.println("Invalid Value");
+            return false;
         }
+
+        int sumOfDividers = -number;
         for (int i = 1; i <= number; i++) {
-            while(number%i==0){
-                System.out.print(i + " ");
-                i++;
+            if(number%i==0){
+                sumOfDividers += i;
+                //System.out.println(i+" ");
             }
         }
+        System.out.println("Sum of dividers " + sumOfDividers);
+
+        boolean isPerfectNumber = (sumOfDividers==number);
+
+        if (isPerfectNumber==true){
+            return true;
+        } else {
+        return false;
     }
+    }
+
+    //perfercNumber = divisor + divisoe
+    //divisor %= perfercrNUmber;
 }
