9d8f39e0b52b51baa1b4dfcdc56378be916cd833
vlad666spot <vladyslav.z@spot.io>
2021-08-25 14:15:34 +0300

    Implemented Print Factors

--- src/Main.java
+++ src/Main.java
@@ -4,53 +4,18 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(getGreatestCommonDivisor(81,153));
+        printFactors(-1);
     }
-
-
-
-    public static int getGreatestCommonDivisor(int first, int second) {
-        if (first < 10 || second < 10) {
-            return -1;
+    
+    public static void printFactors (int number){
+        if(number<1){
+            System.out.println("Invalid Value");
         }
-
-        ArrayList<Integer> firstArray = new ArrayList<Integer>();
-        ArrayList<Integer> secondArray = new ArrayList<Integer>();
-        ArrayList<Integer> biggest = new ArrayList<>();
-        for (int i = 1; i <= first; i++) {
-
-            if (first % i == 0) {
-                firstArray.add(i);
-                //System.out.println(i);
+        for (int i = 1; i <= number; i++) {
+            while(number%i==0){
+                System.out.print(i + " ");
+                i++;
             }
         }
-
-        for (int i = 1; i <= second; i++) {
-
-            if (second % i == 0) {
-                secondArray.add(i);
-                //System.out.println(i);
-            }
-        }
-
-        for (int i = 0; i < firstArray.size(); i++) {
-            int numberOfFirstArray = firstArray.get(i);
-            for (int j = 0; j < secondArray.size(); j++) {
-                int numberOfSecondArray = secondArray.get(j);
-                if (numberOfFirstArray==numberOfSecondArray){
-                    biggest.add(numberOfFirstArray);
-                }
-            }
-        }
-
-        int bigVal = 0;
-        for (int i = 0; i < biggest.size(); i++) {
-            biggest.get(i);
-            if (biggest.get(i)>bigVal){
-                bigVal=biggest.get(i);
-            }
-        }
-
-    return bigVal;
     }
 }
