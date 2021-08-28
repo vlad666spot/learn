23177f68506b063a4353160573aea47af14596a4
vlad666spot <vladyslav.z@spot.io>
2021-08-28 17:06:34 +0300

    Implemented bank account

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Person vlad = new Person();
-        vlad.setAge(-5);
-        vlad.setLastName("Smith");
-        vlad.getFullName();
+        BankAccount one = new BankAccount();
+        one.setBalance(200);
+        one.withdrawFunds(201);
     }
 
 }
