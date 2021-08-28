76964348b704425743063bedc59eae5d39a5ce88
vlad666spot <vladyslav.z@spot.io>
2021-08-28 16:06:23 +0300

    Implemented Methods and Classes

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,12 @@
-import java.util.Scanner;
-
 public class Main {
 
 
     public static void main(String[] args) {
-    inputThenPrintSumAndAverage();
 
+        SimpleCalculator test = new SimpleCalculator();
+        test.setFirstNumber(-1.25);
+        test.setSecondNumber(-1.25);
+        System.out.println(test.getDivisionResult());
     }
 
-    public static void inputThenPrintSumAndAverage (){
-        Scanner scanner = new Scanner(System.in);
-
-        int sum = 0;
-        int counter = 0;
-        while(true){
-            if (scanner.hasNextInt()){
-                int number = scanner.nextInt();
-                scanner.nextLine();
-                sum += number;
-                counter++;
-            } else {
-                if(counter==0){
-
-                    System.out.println("SUM = " + 0 + " AVG = " + 0);
-                    break;
-                }
-                counter++;
-                System.out.println("SUM = " + sum + " AVG = " + Math.round(sum/counter));
-
-                break;
-            }
-        }
-        scanner.close();
-    }
 }
