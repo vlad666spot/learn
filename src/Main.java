94095a7428aaffa42f4dbb3a8382bcce52efdfd8
vlad666spot <vladyslav.z@spot.io>
2021-09-01 12:57:34 +0300

    Created constructor

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ public class Main {
 
 
     public static void main(String[] args) {
-        //BankAccount one = new BankAccount();
-        VipClient vip = new VipClient("Vlad", 2,"Test@s.io");
-        //one.setBalance(200);
-        //one.withdrawFunds(201);
+        Wall test = new Wall(5,4);
+        System.out.println("area: " + test.getArea());
+        System.out.println("width: " + test.getWidth());
+        System.out.println("height: " + test.getHeight());
+        //System.out.println("area: " + test.getArea());
     }
 
 }
