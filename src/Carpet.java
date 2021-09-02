901fef6ca00415d8f4e10b2b68540e0803a1ea0c
vlad666spot <vladyslav.z@spot.io>
2021-09-02 13:42:27 +0300

    fix

--- src/Carpet.java
+++ src/Carpet.java
@@ -4,9 +4,9 @@ public class Carpet {
     public Carpet(double cost){
         if(cost<0){
             this.cost = 0;
+        } else {
+            this.cost=cost;
         }
-
-        this.cost=cost;
     }
 
     public double getCost(){
