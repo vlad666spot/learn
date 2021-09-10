d509157d0288729805ca6e3201acaa01cd9a258e
vlad666spot <vladyslav.z@spot.io>
2021-09-10 12:59:13 +0300

    Quantifiers, pattern and matcher classses

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -44,11 +46,34 @@ public class Main {
         System.out.println(hasWhiteSpace.replaceAll("\\S","")); //remove all NON white space (probil)
         System.out.println(hasWhiteSpace.replaceAll("\\w", "C")); //remove a-z , A-Z, 0-9, apart of white space ( probil )
         System.out.println(hasWhiteSpace.replaceAll("\\W", "C")); //remove all apart of a-z , A-Z, 0-9, ( so white space ( probil ))
-        System.out.println(hasWhiteSpace.replaceAll("\\b","*")); //surround word by char (good to wrap in html tags) 
+        System.out.println(hasWhiteSpace.replaceAll("\\b","*")); //surround word by char (good to wrap in html tags)
+
+        //QUANTIFIER (*)
+        String thirdAlphaNumericString = "abcDeeeF12Ghhjjjjjkl99z";
+        System.out.println(thirdAlphaNumericString.replaceAll("^abcDe{3}", "YYY")); //{3} - amount of how much the last symbol is occuring, in  this case "e"
+        System.out.println(thirdAlphaNumericString.replaceAll("^abcDe+","YYY")); // + indicate that there should be zero or more last symbols , (in this case "e" letter)
+        System.out.println(thirdAlphaNumericString.replaceAll("^abcDe*", "YYY")); // ???
+        System.out.println(thirdAlphaNumericString.replaceAll("^abcDe{2,5}", "YYY")); //e repeats from 2 till 5 times
+        System.out.println(thirdAlphaNumericString.replaceAll("h+j*k", " !Y! ")); //"h" followed by followed by any number of "j", followed at least one "k", replace all that with "Y"
 
 
+        //.replaceAll was used to demonstrate basics of regular expression, but really regular
+        // expressions are used at Pattern class (but some java APIs won't work with patterns), so we need to use
+        // pattern.compile method to compile regular expression into a pattern !!!
 
 
+        //compile regular expression into a pattern
+        StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
+        htmlText.append("<h2>Sub-heading</h2>");
+        htmlText.append("<p>This is a paragraph about something.</p>");
+        htmlText.append("<p>This is another paragraph about something else.</p>");
+        htmlText.append("<h2>Summary</h2>");
+        htmlText.append("<p>Here is summary.</p>");
+
+        String h2Pattern = ".*<h2>.*"; //anything before and anything after h2
+        Pattern pattern = Pattern.compile(h2Pattern);
+        Matcher matcher = pattern.matcher(htmlText);
+        System.out.println(matcher.matches());
 
         String spotTest = " { { s o m e v a l u e } } ";
         System.out.println(spotTest.replaceAll("[{}]", "issue detected"));
