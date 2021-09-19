fd2a4c50e8bc022c7e2a9cd0f587267e401da5f2
vlad666spot <vladyslav.z@spot.io>
2021-09-19 13:27:45 +0300

    empty

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-    Rectangle test = new Rectangle(-5.0,1.0);
-        System.out.println(test.getWidth());
-        System.out.println(test.getLength());
+
     }
 }
