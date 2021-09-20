0fe59953ca12f29ff7f6ac898fed10e5026e2c13
vlad666spot <vladyslav.z@spot.io>
2021-09-20 13:01:49 +0300

    how to use encapsulation

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        
+        Printer hp = new Printer(50,true);
+        System.out.println(hp.addToner(50));
+        System.out.println("Initial page count: "+ hp.getPagesPrinted());
+        int pagesPrinted = hp.printPages(4);
+        System.out.println("Pages printed was "+ pagesPrinted+ " new total print count for printer = "+ hp.getPagesPrinted());
+        pagesPrinted = hp.printPages(2);
+        System.out.println("Pages printed was "+ pagesPrinted+ " new total print count for printer = "+ hp.getPagesPrinted());
     }
 }
