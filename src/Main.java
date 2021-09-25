ec7b5a8448ad1717efa292183a71507419d2c347
vlad666spot <vladyslav.z@spot.io>
2021-09-25 16:21:09 +0300

    solved

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int [] testVlad = getIntegers(5);
-        printArray(sortArray(testVlad));
+        System.out.println("Please fill in counter");
 
-
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        System.out.println(findMin(readIntegers(count)));
     }
 
-    public static int[] getIntegers(int capacity){
-        int[] array = new int[capacity];
-        System.out.println("Please enter "+ capacity + " integer values:\r");
-        for (int i = 0; i < capacity; i++) {
+    public static int[] readIntegers(int count){
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[count];
+        System.out.println("Please fill in int");
+        for (int i = 0; i < count; i++) {
             array[i]=scanner.nextInt();
         }
         return array;
     }
 
-    public static void printArray(int[] array){
+    public static int findMin(int [] array){
+        int minVal = Integer.MAX_VALUE;;
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            if (minVal>array[i]){
+                minVal=array[i];
             }
-      }
-
-    public static int[] sortArray(int[] array){
-        //bubble sorting
-        boolean isSorted = false;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i] < array[i + 1]) {
-                    int temp = array[i + 1];
-                    array[i + 1] = array[i];
-                    array[i] = temp;
-                    isSorted = false;
-                }
-            }
-
         }
-        return array;
+        return minVal;
     }
-}
 
+}
