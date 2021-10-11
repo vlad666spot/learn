c82dfa306a5dd7c5e5377ac470654a766acccf5f
vlad666spot <vladyslav.z@spot.io>
2021-10-11 10:07:21 +0300

    blank

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.util.stream.IntStream;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        MobilePhone lg = new MobilePhone("8765");
-        Contact s = new Contact("SSSS", "09876543");
-        System.out.println(lg.addNewContact(s));
+
     }
+
 }
