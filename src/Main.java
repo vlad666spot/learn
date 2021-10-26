aef575b70f075e52d26c615b466f0c4665ca2239
vlad666spot <vladyslav.z@spot.io>
2021-10-26 17:43:54 +0300

    hashset

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,21 @@
-/*
-    Only add/edit code where it is stated in the description.
-*/
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    private Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    public static void main(String[]args){
 
-    public Main() {
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
+        Set<String> cars = new HashSet<String>();
+        cars.add("Ford");
+        cars.add("Audi");
+        cars.add("Bmw");
+        cars.add("Mercedes");
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
+        System.out.println(cars);
+        System.out.println(cars.contains("Ford"));
+        cars.remove("Ford");
 
-        locations.get(2).addExit("N", 5);
-
-        locations.get(3).addExit("W", 1);
-
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
-
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
-    }
-
-    public void command() {
-        // write code here
-        Scanner scanner = new Scanner(System.in);
-        scanner.next();
+        System.out.println("Is empty ? - "+cars.isEmpty());
+        System.out.println("What size ? - "+cars.size());
+        System.out.println(cars);
     }
 }
